566f13906d32e62eef827a4a7ddd158c190cab13
nmfrancisco14 <nefriendfrancisco@gmail.com>
2025-08-19 23:01:45 +0800

    Add list.names param to get_all_api_data function
    Introduces a new 'list.names' parameter to get_all_api_data, allowing users to specify which dataframes to retrieve from the API. Updates documentation accordingly. Also comments out the 'pay_prism_mon' endpoint in the registry.

--- R/get_alldata.R
+++ R/get_alldata.R
@@ -4,12 +4,13 @@
 #' You can extend this by adding more with `add_api_url()`.
 #'
 #' @param key API key. Default reads from the lakeAPIkey environment variable.
+#' @param list.names list of names of dataframe to get from API. run `list_api_endpoints()` to list all possible API 
 #'
 #' @return A named list of data frames.
 #' @export
-get_all_api_data <- function(key = Sys.getenv("lakeAPIkey")) {
+get_all_api_data <- function(key = Sys.getenv("lakeAPIkey"), list.names = names(.api_url_registry)) {
   # Get all current API names in the registry
-  api_names <- names(.api_url_registry)
+  api_names <- list.names
 
   # Retrieve data from each endpoint
   data_list <- purrr::map(api_names, ~ get_api_data(.x, key = key, 
