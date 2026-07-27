ebde8282a568f128885c58b37d14a4a17b28a946
nmfrancisco14 <nefriendfrancisco@gmail.com>
2026-07-27 10:41:11 +0800

    new registry and message

--- R/zzz.R
+++ R/zzz.R
@@ -2,4 +2,5 @@
   packageStartupMessage("📦 Package 'daclakeapi' loaded successfully!")
   packageStartupMessage("🔗 This package provides API functions to retrieve and summarize external data.")
   packageStartupMessage("💡 Use `?get_all_api_data` to see how to start.")
+  packageStartupMessage("© 2026 PhilRice Rice Data Analytics Center. All rights reserved.")
 }
